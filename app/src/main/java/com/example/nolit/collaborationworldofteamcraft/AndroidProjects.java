e6a6cee095c379559d21798b6686be78bf44834b
lixir <pokaprivet10@gmail.com>
2018-05-16 22:58:55 +0300

    Create AndroidProjects.java

---
+++ app/src/main/java/com/example/nolit/collaborationworldofteamcraft/AndroidProjects.java
@@ -0,0 +1,41 @@
+package com.example.nolit.collaborationworldofteamcraft;
+
+/**
+ * Created by lixir on 16.05.18.
+ */
+
+
+import java.util.ArrayList;
+
+public class AndroidProjects {
+    private int idProject;
+    private String nameProject;
+    private int leaderProject;
+    private ArrayList<Users> usersProject;
+
+    public AndroidProjects() {
+    }
+
+    public AndroidProjects(int id, String name, int leader, ArrayList<Users> listUsers){
+        this.idProject=id;
+        this.leaderProject=leader;
+        this.nameProject=name;
+        this.usersProject=listUsers;
+    }
+
+    public int getIdProject() {
+        return idProject;
+    }
+
+    public String getNameProject() {
+        return nameProject;
+    }
+
+    public int getLeaderProject() {
+        return leaderProject;
+    }
+
+    public ArrayList<Users> getUsersProject() {
+        return usersProject;
+    }
+}
